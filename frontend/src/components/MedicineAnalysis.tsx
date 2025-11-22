9ac7a18a2b86e57a1e9362be718821c5557daa9d
Deepanshu9644 <dpaliwal9644@gmail.com>
2025-11-22 18:49:27 +0530

    Update MedicineAnalysis.tsx

--- frontend/src/components/MedicineAnalysis.tsx
+++ frontend/src/components/MedicineAnalysis.tsx
@@ -86,13 +86,6 @@ const handleAnalyze = async () => {
   }
 };
 
-const scrollToSection = (id: string) => {
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
 
 
 
